fe2441ba9f35fbc31b6f7d77f0d6f5d05717b898
allenmurfee <allenmurfee123@gmail.com>
2023-07-18 19:58:58 -0400

    working on 'mainLoop' function

--- script.js
+++ script.js
@@ -1,9 +1,15 @@
 let lastRenderTime = 0;
 
+const snakeSpeed = 2;
+
 //Game loop function
 function mainLoop(currentTime) {
   window.requestAnimationFrame(mainLoop);
-  console.log(currentTime);
+  const secondsSinceLastRender = (currentTime - lastRenderTime) / 1000;
+  console.log(secondsSinceLastRender);
+
+  if (secondsSinceLastRender < 1 / snakeSpeed) return;
+  console.log("render");
   lastRenderTime = currentTime;
 }
 
