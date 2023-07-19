8cee68e6737227a8d4ca06a2ec2eb9d4cf221b94
allenmurfee <allenmurfee123@gmail.com>
2023-07-18 20:04:38 -0400

    added js file 'snake' for snake js and exported snake speed - also created draw and update functions

--- script.js
+++ script.js
@@ -1,16 +1,23 @@
 let lastRenderTime = 0;
-
-const snakeSpeed = 2;
+import { snakeSpeed } from "./snake";
 
 //Game loop function
 function mainLoop(currentTime) {
   window.requestAnimationFrame(mainLoop);
   const secondsSinceLastRender = (currentTime - lastRenderTime) / 1000;
-  console.log(secondsSinceLastRender);
+  //   console.log(secondsSinceLastRender);
 
   if (secondsSinceLastRender < 1 / snakeSpeed) return;
   console.log("render");
   lastRenderTime = currentTime;
+
+  //call 2 functions
+  update();
+  draw();
 }
 
 window.requestAnimationFrame(mainLoop);
+
+function update() {}
+
+function draw() {}
